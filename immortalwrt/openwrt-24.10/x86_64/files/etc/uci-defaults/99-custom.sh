d2a1aaf98ea5f800d7466ca32d091dce4c209872
root <root@Debian12-CT-Server.lan>
2025-02-18 17:03:49 +0800

    更新文件: 同步目标主机配置

--- immortalwrt/openwrt-24.10/x86_64/files/etc/uci-defaults/99-custom.sh
+++
@@ -1,7 +0,0 @@
-# 设置 root 用户的密码
-root_password="PayCRNTbzRu3ypsXL@892515357"
-if [ -n "$root_password" ]; then
-  (echo "$root_password"; sleep 1; echo "$root_password") | passwd > /dev/null
-fi
-
-uci set system.@system[0].hostname='XWRT'
