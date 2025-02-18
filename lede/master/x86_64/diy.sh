f38b64d02f754462c2f94ff5ceefc7d5cdc92fb7
passbysuper <superpassby@126.com>
2025-02-18 21:24:49 +0800

    Create diy.sh

---
+++ lede/master/x86_64/diy.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+git clone https://github.com/zxl78585/luci-app-autoreboot.git package/luci-app-autoreboot
+git clone https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
+git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
+git clone https://github.com/vernesong/OpenClash.git --depth=1 package/luci-app-openclash
